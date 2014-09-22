5fe879c3a9d8e92e8acd433283190391ace79311
tenshiemi <tenshiemi@gmail.com>
2014-09-22 17:16:05 +0200

    Update series_master.rb

--- series_master.rb
+++ series_master.rb
@@ -1,24 +1,26 @@
+require 'imdb'
+
 class SeriesMasterOfTheUniverse
 	attr_accessor :series
 
 	def initialize
-		@series = [TVSeries.new("Game of Thrones", 9, "John"), TVSeries.new("RuPaul's Drag Race", 10, "RuPaul"), TVSeries.new("Dexter", 9, "Michael Hall")]
+		@series = [TVSeries.new("Game of Thrones"), TVSeries.new("RuPaul's Drag Race"), TVSeries.new("Dexter")]
 	end
 
 	def sort_by_rating
 		sorted_movies = []
-		@series.sort_by {|x| x.rating }.each {|x| sorted_movies << x}
+		@series.sort_by { |x| x.series_rating }.reverse.each {|x| sorted_movies << x}
 		return sorted_movies
 	end
 
 	def print_movies_sorted_by_rating
 		sort_by_rating.each_with_index do |x, index|
-			puts (index + 1).to_s + ". " + x.title + "  " + x.rating.to_s
+			puts (index + 1).to_s + ". " + x.title + "  " + x.series_rating.to_s
 		end
 	end
 
 	def which_is_better(series1, series2)
-		if series1.rating > series2.rating then return series1.title
+		if series1.series_rating > series2.series_rating then return series1.title
 		else return series2.title
 		end
 	end
@@ -26,13 +28,16 @@ class SeriesMasterOfTheUniverse
 end
 
 class TVSeries
-	attr_accessor :title, :rating, :actor
+	attr_accessor :title, :series_rating, :actor
 
-	def initialize(title, rating, actor)
+	def initialize(title)
 		@title = title
-		@rating = rating
-		@actor = actor
+		search_result = Imdb::Search.new(title).movies.first
+		@series_rating = search_result.rating
+		@actor = search_result.cast_members.first
+		@content_rating = search_result.mpaa_rating
 	end
 end
 
-SeriesMasterOfTheUniverse.new
+tv = SeriesMasterOfTheUniverse.new
+tv.print_movies_sorted_by_rating
