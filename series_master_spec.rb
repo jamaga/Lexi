41c24802b731ec1aa3a6f847aa9f476cadc22805
tenshiemi <tenshiemi@gmail.com>
2014-09-22 17:16:25 +0200

    Update series_master_spec.rb

--- series_master_spec.rb
+++ series_master_spec.rb
@@ -7,15 +7,27 @@ describe SeriesMasterOfTheUniverse do
 
 	describe '#sort_by_rating' do
 		it 'It sorts the list of TV Series by rating' do
-			expect(@series_object.sort_by_rating.map{|x| x.rating}).to eq([9, 9, 10])
+			expect(@series_object.sort_by_rating.map{|x| x.series_rating}).to eq([9.5, 9.0, 7.8])
 		end
 	end
 
 	describe '#which_is_better' do
 		it 'It will compare the ratings of two TV Series' do
-			better_series = TVSeries.new("RuPaul's Drag Race", 10, "RuPaul")
-			worse_series = TVSeries.new("Game of Thrones", 9, "John")
-			expect(@series_object.which_is_better(worse_series, better_series)).to eq("RuPaul's Drag Race")
+			worse_series = TVSeries.new("RuPaul's Drag Race")
+			better_series = TVSeries.new("Game of Thrones")
+			expect(@series_object.which_is_better(worse_series, better_series)).to eq("Game of Thrones")
+		end
+	end
+
+	describe '#most_famous_cast_member' do
+		it 'It announces who the most famous cast member in the TV series is' do
+			(TVSeries.new("Friends")).should == "Jennifer Aniston"
+		end
+	end
+
+	describe '#sort_by_mpaa_rating' do
+		it 'Sorts by the MPAA rating of the TV series' do
+			TVSeries.new("Game of Thrones").should == 'R'
 		end
 	end
 end
@@ -24,13 +36,13 @@ describe TVSeries do
 
 	describe '#initialize' do
 		it "Creates a new object with a title" do
-		expect(TVSeries.new("Friends", 7, "Jennifer Aniston").title).to eq("Friends")
+		expect(TVSeries.new("Friends").title).to eq("Friends")
 		end
 		it "Creates a new object with a rating" do
-		expect(TVSeries.new("Friends", 7, "Jennifer Aniston").rating).to eq(7)
+		expect(TVSeries.new("Friends").series_rating).to eq(9.0)
 		end
-		it "Creates a new object with a title" do
-		expect(TVSeries.new("Friends", 7, "Jennifer Aniston").actor).to eq("Jennifer Aniston")
+		it "Creates a new object with a cast member" do
+		expect(TVSeries.new("Friends").actor).to eq("Jennifer Aniston")
 		end
 	end
 end
